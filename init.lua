db7250105ab2064ebff289bf9f6b65af587db426
GG <weebcyberpunk@gmail.com>
2022-06-21 22:56:43 -0300

    Fixed bug on bootstrapping

--- init.lua
+++ init.lua
@@ -19,12 +19,12 @@
 --      | |\ \       || )
 --     (_/ (_/      ((_/
 
-require('impatient')
-
 local fn = vim.fn
 local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
 if fn.empty(fn.glob(install_path)) > 0 then
 	packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+else
+	require('impatient')
 end
 
 require('packer').startup({function(use)
