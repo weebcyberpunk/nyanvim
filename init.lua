9c998a4b616499c753992dcb6eb58064c58e481c
GG <weebcyberpunk@gmail.com>
2022-06-23 17:19:34 -0300

    Moved lsp and completion settings to packer handler

--- init.lua
+++ init.lua
@@ -145,14 +145,85 @@ require('packer').startup(function(use)
 	}
 	use 'simrat39/rust-tools.nvim'
 	use 'p00f/clangd_extensions.nvim'
-	use 'neovim/nvim-lspconfig'
-	use 'hrsh7th/cmp-nvim-lsp'
-	use 'hrsh7th/cmp-buffer'
-	use 'hrsh7th/cmp-path'
-	use 'hrsh7th/cmp-cmdline'
-	use 'hrsh7th/nvim-cmp'
-	use 'L3MON4D3/LuaSnip'
-	use 'saadparwaiz1/cmp_luasnip'
+	use { 'hrsh7th/nvim-cmp', requires = {
+		'neovim/nvim-lspconfig',
+		'hrsh7th/cmp-nvim-lsp',
+		'hrsh7th/cmp-buffer',
+		'hrsh7th/cmp-path',
+		'hrsh7th/cmp-cmdline',
+		'L3MON4D3/LuaSnip',
+		'saadparwaiz1/cmp_luasnip',
+	}, config = function() 
+		-- LSP AND COMPLETION SETTINGS {{{
+		local cmp = require'cmp'
+		cmp.setup({
+			snippet = {
+				expand = function(args)
+					require('luasnip').lsp_expand(args.body)
+				end,
+			},
+			mapping = cmp.mapping.preset.insert({
+				['<C-d>'] = cmp.mapping.scroll_docs(-4),
+				['<C-f'] = cmp.mapping.scroll_docs(4),
+				['<C-g>'] = cmp.mapping.complete(),
+				['<C-Space>'] = cmp.mapping.confirm {
+					behavior = cmp.ConfirmBehavior.Replace,
+					select = true,
+				},
+				['<Tab>'] = cmp.mapping(function(fallback)
+					if cmp.visible() then
+						cmp.select_next_item()
+					else
+						fallback()
+					end
+				end, { 'i', 's' }),
+				['<S-Tab>'] = cmp.mapping(function(fallback)
+					if cmp.visible() then
+						cmp.select_prev_item()
+					else
+						fallback()
+					end
+				end, { 'i', 's' }),
+			}),
+			sources = cmp.config.sources({
+				{ name = 'nvim_lsp' },
+				{ name = 'luasnip' },
+			}, {
+				{ name = 'buffer' },
+			}),
+		})
+
+		cmp.setup.filetype('gitcommit', {
+			sources = cmp.config.sources({
+				{ name = 'cmp_git' },
+			}, {
+				{ name = 'buffer' },
+			})
+		})
+
+		cmp.setup.cmdline('/', {
+			mapping = cmp.mapping.preset.cmdline(),
+			sources = {
+				{ name = 'buffer' }
+			}
+		})
+		cmp.setup.cmdline(':', {
+			mapping = cmp.mapping.preset.cmdline(),
+			sources = cmp.config.sources({
+				{ name = 'path' }
+			}, {
+				{ name = 'cmdline' }
+			})
+		})
+
+		local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+		require('lspconfig')['pyright'].setup { capabilities = capabilities }
+
+		require('rust-tools').setup()
+		require('clangd_extensions').setup()
+-- }}}
+	end
+	}
 	-- }}}
 
 	-- tetris
@@ -308,73 +379,4 @@ vim.api.nvim_create_autocmd({'FileType'}, {
 		vim.wo.signcolumn = 'no'
 	end
 })
--- }}}
-
--- LSP AND COMPLETION {{{
-local cmp = require'cmp'
-cmp.setup({
-	snippet = {
-		expand = function(args)
-			require('luasnip').lsp_expand(args.body)
-		end,
-	},
-	mapping = cmp.mapping.preset.insert({
-		['<C-d>'] = cmp.mapping.scroll_docs(-4),
-		['<C-f'] = cmp.mapping.scroll_docs(4),
-		['<C-g>'] = cmp.mapping.complete(),
-		['<C-Space>'] = cmp.mapping.confirm {
-			behavior = cmp.ConfirmBehavior.Replace,
-			select = true,
-		},
-		['<Tab>'] = cmp.mapping(function(fallback)
-			if cmp.visible() then
-				cmp.select_next_item()
-			else
-				fallback()
-			end
-		end, { 'i', 's' }),
-		['<S-Tab>'] = cmp.mapping(function(fallback)
-			if cmp.visible() then
-				cmp.select_prev_item()
-			else
-				fallback()
-			end
-		end, { 'i', 's' }),
-	}),
-	sources = cmp.config.sources({
-		{ name = 'nvim_lsp' },
-		{ name = 'luasnip' },
-	}, {
-		{ name = 'buffer' },
-	}),
-})
-
-cmp.setup.filetype('gitcommit', {
-	sources = cmp.config.sources({
-		{ name = 'cmp_git' },
-	}, {
-		{ name = 'buffer' },
-	})
-})
-
-cmp.setup.cmdline('/', {
-	mapping = cmp.mapping.preset.cmdline(),
-	sources = {
-		{ name = 'buffer' }
-	}
-})
-cmp.setup.cmdline(':', {
-	mapping = cmp.mapping.preset.cmdline(),
-	sources = cmp.config.sources({
-		{ name = 'path' }
-	}, {
-		{ name = 'cmdline' }
-	})
-})
-
-local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
-require('lspconfig')['pyright'].setup { capabilities = capabilities }
-
-require('rust-tools').setup()
-require('clangd_extensions').setup()
 -- }}}
