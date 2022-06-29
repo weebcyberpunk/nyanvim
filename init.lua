0384066008431d4861ea18d99ac2ce4bfaab3a14
GG <weebcyberpunk@gmail.com>
2022-06-29 20:08:23 -0300

    Minor tweaks in colorscheme

--- init.lua
+++ init.lua
@@ -64,8 +64,29 @@ require('packer').startup(function(use)
 				strings = 'NONE',
 				variables = 'NONE',
 			},
-			transparent_background = true,
 			term_colors = true,
+			integrations = {
+				native_lsp = {
+					enabled = true,
+					virtual_text = {
+						errors = "italic",
+						hints = "italic",
+						warnings = "italic",
+						information = "italic",
+					},
+					underlines = {
+						errors = "underline",
+						hints = "underline",
+						warnings = "underline",
+						information = "underline",
+					},
+				},
+				cmp = true,
+				gitsigns = true,
+				lsp_trouble = true,
+				treesitter = true,
+
+			},
 		})
 		vim.g.catppuccin_flavour = "mocha"
 		vim.cmd('colorscheme catppuccin')
