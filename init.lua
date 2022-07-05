492f7313598ef1db2f02305bfcf52226a82bb872
GG <weebcyberpunk@gmail.com>
2022-07-04 21:22:12 -0300

    Improved netrw configs

--- init.lua
+++ init.lua
@@ -266,14 +266,13 @@ vim.opt.mouse = "a"
 -- }}}
 
 -- NETRW {{{
-
-	vim.g.netrw_banner 		= 0
-	vim.g.netrw_keepdir 		= 0
-	vim.g.netrw_liststyle 		= 3
-	vim.g.netrw_localrmdir 		= 'rm'
-	vim.g.netrw_localrmdiropt 	= '-r'
-	vim.g.netrw_sizestyle 		= 'H'
-	vim.g.netrw_specialsyntax 	= true
+vim.g.netrw_banner 		= 0
+vim.g.netrw_keepdir 		= 0
+vim.g.netrw_localrmdir 		= 'rm'
+vim.g.netrw_localrmdiropt 	= '-rf'
+vim.g.netrw_sizestyle 		= 'H'
+vim.g.netrw_special_syntax 	= true
+vim.g.netrw_use_errorwindow 	= 0
 -- }}}
 
 -- KEYBINDS AND COMMANDS {{{
