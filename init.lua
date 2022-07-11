333d69fb15e81f0a4897d83e2e02871d3683876c
GG <weebcyberpunk@gmail.com>
2022-07-11 19:00:01 -0300

    Removed autosethints = false

--- init.lua
+++ init.lua
@@ -237,8 +237,8 @@ require('packer').startup(function(use)
 
         local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
         require('lspconfig')['pyright'].setup({capabilities = capabilities})
-        require('rust-tools').setup({ autoSetHints = false, })
-        require('clangd_extensions').setup({ autoSetHints = false, })
+        require('rust-tools').setup()
+        require('clangd_extensions').setup()
 -- }}}
     end
     }
